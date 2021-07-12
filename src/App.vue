1eda8da8b1c6f43d4d8ba20ceec5c71c66b805ab
pyj <puyijun910@gmail.com>
2021-07-12 22:28:03 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <el-container>
+        <el-header class="header">
+            <el-page-header content="详情页面"></el-page-header>
+        </el-header>
+        <router-view />
+        <el-footer>
+			
+		</el-footer>
+    </el-container>
+</template>
+
+<style lang="scss">
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+
+#nav {
+    padding: 30px;
+
+    a {
+        font-weight: bold;
+        color: #2c3e50;
+
+        &.router-link-exact-active {
+            color: #42b983;
+        }
+    }
+}
+.header{
+	background-color: #2c3e50;
+}
+</style>
