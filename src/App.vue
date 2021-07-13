593b989f9a55077e2398c1373f0ec71e7921b25f
pyj <puyijun910@gmail.com>
2021-07-13 22:26:16 +0800

    头部和左侧菜单栏封装组件

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,8 @@
 <template>
-  <el-container class="main-container">
-    <el-header class="header">
-      <el-row>
-        <el-col :span="2">
-          <h3>夏日粥品</h3>
-        </el-col>
-        <el-col :span="19"></el-col>
-        <el-col :span="3">
-			<el-row>
-				<el-col :span="12">
-					<p>admin</p>
-				</el-col>
-				<el-col :span="12">
-					<el-avatar :size="45" :src="circleUrl" class="tx-img"></el-avatar>
-				</el-col>
-			</el-row>
-        </el-col>
-      </el-row>
-    </el-header>
-    <router-view />
-  </el-container>
+  <router-view />
 </template>
 
 <style lang="scss">
-html,
-body,
-#app {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-}
-.main-container {
-  height: 100%;
-  .header {
-    background-color: #2c3e50;
-    color: white;
-	.tx-img{
-		margin-top: 7px;
-	}
-  }
-}
+@import "./assets/css/main.css";
+@import "./assets/css/color-dark.css";
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,89 +1,50 @@
 <template>
-  <el-container class="home-container">
-    <el-aside>
-      <el-menu
-        :uniqueOpened="true"
-        default-active="shop"
-        class="main-menu"
-        @open="handleOpen"
-        @close="handleClose"
-        background-color="#545c64"
-        text-color="#fff"
-        active-text-color="#ffd04b"
-        router
-      >
-        <el-menu-item index="1">
-          <template #title>统计</template>
-        </el-menu-item>
-        <el-menu-item index="user">
-          <template #title>客户管理</template>
-        </el-menu-item>
-        <el-menu-item index="shop">
-          <template #title>商品管理</template>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <template #title>导航四</template>
-        </el-menu-item>
-        <el-submenu index="5">
-          <template #title>
-            <span>导航一</span>
-          </template>
-          <el-menu-item-group>
-            <template #title>分组一</template>
-            <el-menu-item index="5-1">选项1</el-menu-item>
-            <el-menu-item index="5-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="5-3">选项3</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-      </el-menu>
-    </el-aside>
-    <el-container>
-      <el-header>
-        <el-breadcrumb separator-class="el-icon-arrow-right" class="top-breadcrumb">
-          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item>活动管理</el-breadcrumb-item>
-          <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-          <el-breadcrumb-item>活动详情</el-breadcrumb-item>
-        </el-breadcrumb>
-      </el-header>
-      <!-- 内容区 -->
-      <router-view></router-view>
+    <el-container class="home-container">
+        <v-header />
+        <el-container>
+            <v-sidebar />
+            <el-container>
+                <el-header>
+                    <el-breadcrumb separator-class="el-icon-arrow-right" class="top-breadcrumb">
+                        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+                        <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+                        <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+                        <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+                    </el-breadcrumb>
+                </el-header>
+                <!-- 内容区 -->
+                <router-view></router-view>
+            </el-container>
+        </el-container>
     </el-container>
-  </el-container>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import VHeader from "../components/Header.vue";
+import VSidebar from "../components/Sidebar.vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-  methods: {
-    handleOpen(key: any, keyPath: any) {
-      console.log(key, keyPath);
+    name: "Home",
+    components: {
+        HelloWorld,
+        VHeader,
+        VSidebar,
     },
-    handleClose(key: any, keyPath: any) {
-      console.log(key, keyPath);
+    methods: {
+        handleOpen(key: any, keyPath: any) {
+            console.log(key, keyPath);
+        },
+        handleClose(key: any, keyPath: any) {
+            console.log(key, keyPath);
+        },
     },
-  },
 });
 </script>
 
 <style lang="scss">
 .home-container {
-  margin-top: 6px;
-  background-color: aquamarine;
-  .main-menu {
-    height: 100%;
-  }
-  .top-breadcrumb {
-    margin-top: 5px;
-  }
 
 }
 </style>
